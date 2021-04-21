752711f2d9a60ed6dbcc3086b2ee1310896aac5f
noa bayer <noa.bayer.s2@gmail.com>
2021-04-21 16:20:36 +0300

    adding 31 32 recording

--- Training/Script_Noa.m
+++ Training/Script_Noa.m
@@ -8,7 +8,7 @@ addpath 'C:\ToolBoxes\eeglab2020_0\plugins\xdfimport1.14\xdf-EEGLAB'
 eeglab;  
 
 
-electrodesToRemove = [];
+electrodesToRemove = [1, 2, 7, 8];
 plotLowPassHighPassFreqResp = 0;
 plotScroll = 0;
 plotSpectraMaps = 0;
@@ -26,31 +26,37 @@ pauseAfterEachTrain = 0;
 FeatureSelectMode = 0;
 Features2Select = 5;
 Feature2SelectFile = '';
-learnModel = 0; % 0 - lda 1 - svm rbf 2 - AdaBoostM2
+learnModel = 1; % 0 - lda 1 - svm rbf 2 - AdaBoostM2
 cv = 1;
 saveModel = 1;
 % Maybe extract more parameters out of the functions
 
-recordingFolder1 = 'C:\master\bci\recording-21-4\Sub21\';
-recordingFolder2 = 'C:\master\bci\recording-21-4\Sub22\';
-recordingFolder3 = 'C:\master\bci\recording-21-4\Sub23\';
+recordingFolder1 = 'C:\master\bci\recording-21-4\Sub31\';
+recordingFolder2 = 'C:\master\bci\recording-21-4\Sub32\';
+%recordingFolder3 = 'C:\master\bci\recording-21-4\Sub23\';
 
 MI2_Preprocess_Scaffolding(recordingFolder1, electrodesToRemove, useLowPassHighPass, useNotchHighPass, plotLowPassHighPassFreqResp, plotScroll, plotSpectraMaps, resampleFsHz, automaticNoiseRejection, automaticAverageReReference);
 disp('Preprocess 1 done...');
+if pauseAfterEachPreprocess == 1
+    pause;
+end
 MI2_Preprocess_Scaffolding(recordingFolder2, electrodesToRemove, useLowPassHighPass, useNotchHighPass, plotLowPassHighPassFreqResp, plotScroll, plotSpectraMaps, resampleFsHz, automaticNoiseRejection, automaticAverageReReference);
 disp('Preprocess 2 done...');
-MI2_Preprocess_Scaffolding(recordingFolder3, electrodesToRemove, useLowPassHighPass, useNotchHighPass, plotLowPassHighPassFreqResp, plotScroll, plotSpectraMaps, resampleFsHz, automaticNoiseRejection, automaticAverageReReference);
-disp('Preprocess 3 done...');
+if pauseAfterEachPreprocess == 1
+    pause;
+end
+%MI2_Preprocess_Scaffolding(recordingFolder3, electrodesToRemove, useLowPassHighPass, useNotchHighPass, plotLowPassHighPassFreqResp, plotScroll, plotSpectraMaps, resampleFsHz, automaticNoiseRejection, automaticAverageReReference);
+%disp('Preprocess 3 done...');
 
 MI3_SegmentData_Scaffolding(recordingFolder1);
 MI3_SegmentData_Scaffolding(recordingFolder2);
-MI3_SegmentData_Scaffolding(recordingFolder3);
+%MI3_SegmentData_Scaffolding(recordingFolder3);
 
 MI4_ExtractFeatures_Scaffolding(recordingFolder1, '', 0.2, FeatureSelectMode, Features2Select, Feature2SelectFile, 0, plotSpectrom, plotSpectogram, plotBins, plotBinsFeaturesSelected);
 MI5_LearnModel_Scaffolding(recordingFolder1, learnModel, cv, saveModel);
 MI4_ExtractFeatures_Scaffolding(recordingFolder2, recordingFolder1, 0.2, FeatureSelectMode, Features2Select, Feature2SelectFile, 0, plotSpectrom, plotSpectogram, plotBins, plotBinsFeaturesSelected);
 MI5_LearnModel_Scaffolding(recordingFolder2, learnModel, cv, saveModel);
-MI4_ExtractFeatures_Scaffolding(recordingFolder3, recordingFolder2, 0.2, FeatureSelectMode, Features2Select, Feature2SelectFile, 0, plotSpectrom, plotSpectogram, plotBins, plotBinsFeaturesSelected);
-MI5_LearnModel_Scaffolding(recordingFolder3, learnModel, cv, saveModel);
+%MI4_ExtractFeatures_Scaffolding(recordingFolder3, recordingFolder2, 0.2, FeatureSelectMode, Features2Select, Feature2SelectFile, 0, plotSpectrom, plotSpectogram, plotBins, plotBinsFeaturesSelected);
+%MI5_LearnModel_Scaffolding(recordingFolder3, learnModel, cv, saveModel);
 
 close all;
